24178582fb94d58a87a7edf553923fd68d978170
Jing Yang <jingyang.carl.work@gmail.com>
2020-02-27 15:45:51 -0800

    Update Script2RenderMultiCamFromScene.py

--- Blender/Script2RenderMultiCamFromScene.py
+++ Blender/Script2RenderMultiCamFromScene.py
@@ -13,7 +13,7 @@ import numpy
 #
 # See notes on this in 
 # blender.stackexchange.com/questions/15102/what-is-blenders-camera-projection-matrix-model
-def get_calibration_matrix_K_from_blender(camd):
+def get_K_from_blender(camd):
     f_in_mm = camd.lens
     scene = bpy.context.scene
     resolution_x_in_px = scene.render.resolution_x
@@ -49,86 +49,31 @@ def get_calibration_matrix_K_from_blender(camd):
     return K
 
 # Returns camera rotation and translation matrices from Blender.
-# 
-# There are 3 coordinate systems involved:
-#    1. The World coordinates: "world"
-#       - right-handed
-#    2. The Blender camera coordinates: "bcam"
-#       - x is horizontal
-#       - y is up
-#       - right-handed: negative z look-at direction
-#    3. The desired computer vision camera coordinates: "cv"
-#       - x is horizontal
-#       - y is down (to align to the actual pixel coordinates 
-#         used in digital images)
-#       - right-handed: positive z look-at direction
-def get_3x4_RT_matrix_from_blender(cam):
-    # bcam stands for blender camera
-    R_bcam2cv = Matrix(
-        ((1, 0,  0),
-         (0, -1, 0),
-         (0, 0, -1)))
-
-    # Transpose since the rotation is object rotation, 
-    # and we want coordinate rotation
-    # R_world2bcam = cam.rotation_euler.to_matrix().transposed()
-    # T_world2bcam = -1*R_world2bcam * location
-    #
-    # Use matrix_world instead to account for all constraints
-    location, rotation = cam.matrix_world.decompose()[0:2]
-    R_world2bcam = rotation.to_matrix().transposed()
-
-    # Convert camera location to translation vector used in coordinate changes
-    # T_world2bcam = -1*R_world2bcam*cam.location
-    # Use location from matrix_world to account for constraints:     
-    T_world2bcam = -1*R_world2bcam @ location
-
-    # Build the coordinate transform matrix from world to computer vision camera
-    R_world2cv = R_bcam2cv@R_world2bcam
-    T_world2cv = R_bcam2cv@T_world2bcam
-
-    # put into 3x4 matrix
-    RT = Matrix((
-        R_world2cv[0][:] + (T_world2cv[0],),
-        R_world2cv[1][:] + (T_world2cv[1],),
-        R_world2cv[2][:] + (T_world2cv[2],)
-         ))
-    return RT
+def get_RT_from_blender(cam):
+    T, R = cam.matrix_world.decompose()[0:2]
+    return T, R
 
 def get_3x4_P_matrix_from_blender(cam):
-    K = get_calibration_matrix_K_from_blender(cam.data)
-    RT = get_3x4_RT_matrix_from_blender(cam)
-    return K@RT, K, RT
+    K = get_K_from_blender(cam.data)
+    T, R = get_RT_from_blender(cam)
+    return K, T, R
 
 #---------------------------------------------------------------
 # main
 #---------------------------------------------------------------
-outputPath = 'F:\Project\Blender\CameraStage\Camera Stage V2.1\model\\'
+outputPath = '/home/ICT2000/jyang/Documents/Project/DepthNoise/rendered_clean/'
+outputFormat = '.png'
+cameras_RT = {}
+cameras_K = {}
+cameras = bpy.data.cameras
 
-cameraNames = {
-    0 : 'D415.R(+22.5d).up',
-    1 : 'D415.R(+67.5d).up',
-    2 : 'D415.R(+112.5d).up',
-    3 : 'D415.R(+157.5d).up',
-    4 : 'D415.R(-157.5d).up',
-    5 : 'D415.R(-112.5d).up',
-    6 : 'D415.R(-67.5d).up',
-    7 : 'D415.R(-22.5d).up',
-    8 : 'D415.R(+22.5d).down',
-    9 : 'D415.R(+67.5d).down',
-    10 : 'D415.R(+112.5d).down',
-    11 : 'D415.R(+157.5d).down',
-    12 : 'D415.R(-157.5d).down',
-    13 : 'D415.R(-112.5d).down',
-    14 : 'D415.R(-67.5d).down',
-    15 : 'D415.R(-22.5d).down',
-}
-
-for i in cameraNames : 
+# loop through each camera
+for i, camera in enumerate(cameras) : 
+    # debug purpose
+    
     # get current camera
-    cameraName = cameraNames[i]
-    camera = bpy.context.scene.objects[cameraName]
-    print('Render using camera: ' + cameraName)
+    camera = bpy.context.scene.objects[camera.name]
+    print('Render using camera: ' + camera.name)
     
     # deselect all objects
     bpy.ops.object.select_all(action='DESELECT')
@@ -139,23 +84,41 @@ for i in cameraNames :
     # select the current camera
     camera.select_set(True)
     
-    # set active object as camera
+    # set active object as cameratype
     bpy.context.scene.camera = camera
     
     # set path to save rendered results
-    bpy.context.scene.render.filepath = outputPath + cameraName
+    outputFile = outputPath + camera.name + outputFormat
     
     ## render image
-    print('Rendering model ' + cameraName)
-    bpy.ops.render.render(write_still=True)
+    print('Rendering model ' + camera.name)
+    bpy.ops.render.render()
     
-    # get R, K, RT
-    P, K, RT = get_3x4_P_matrix_from_blender(camera)
-    numpy.savetxt(outputPath+cameraName+'_intrinsic.txt', numpy.matrix(K))
-    print('intrinsic parameter saved')
+    ## save RGB image
+    if 'RGB' in camera.name:
+        bpy.data.images['Viewer Node'].save_render(outputFile)
+    ## save Depth image
+    if 'IR' in camera.name:
+        bpy.data.images['Render Result'].save_render(outputFile)
+    
+    print("Image saved at" + bpy.context.scene.render.filepath)
+    
+    # get K, R, T and print
+    K, T, R = get_3x4_P_matrix_from_blender(camera)
     print(K)
-    numpy.savetxt(outputPath+cameraName+'_extrinsic.txt', numpy.matrix(RT))
+    print(T)
+    print(R)
+    
+    # save Ks, and RTs
+    cameras_K[camera.name] = numpy.matrix(K).A1.tolist()
+    cameras_RT[camera.name] = numpy.concatenate((R, T))[None].flatten()
+    
+    # save
+    numpy.savetxt(outputPath+camera.name+'_K.txt', cameras_K[camera.name])
+    print('intrinsic parameter saved')
+    numpy.savetxt(outputPath+camera.name+'_RT.txt', cameras_RT[camera.name])
     print('extrinsic parameter saved')
-    print(RT)
     
-    
+# save RTs and Ks
+numpy.savetxt(outputPath+'intrinsic.txt', list(cameras_K.values()))
+numpy.savetxt(outputPath+'extrinsic.txt', list(cameras_RT.values()))
