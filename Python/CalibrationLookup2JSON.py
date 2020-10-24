1ee5593be7075c07e98d8c089791c235f9a84f83
Jing Yang <jyang@smtp.ict.usc.edu>
2020-10-23 17:34:13 -0700

    add sh

--- Python/CalibrationLookup2JSON.py
+++ Python/CalibrationLookup2JSON.py
@@ -13,6 +13,7 @@
 # ├── Envrion_10_model_0_camview_00170_cameraDis_13_00001_0.jpg
 # ├── Envrion_10_model_0_camview_00309_cameraDis_10_00005_0.jpg
 # ...
+# ------------------------#
 # ./camera
 # ├── 01
 # │   ├── camview_00001_distance_01.txt
@@ -20,6 +21,7 @@
 # │   ...
 # ├── 02
 # ...
+# ------------------------#
 # ./camview_00001_distance_01.txt
 # │ #focal length
 # │ 1425.928935 1425.928935
@@ -37,6 +39,14 @@
 # │ 0.018531 0.999808 -0.006315 1.043762
 # │ -0.182685 0.009596 0.983125 16.834641
 # │ 0.000000 0.000000 0.000000 1.000000
+# ------------------------#
+# ./spherical_harmonics
+# ├── environment_in_order.txt # a list of spherical harmonics that can be found in ./original_sh
+# └── original_sh
+#     ├── 03-Ueno-Shrine_3k.txt
+#     ├── 10-Shiodome_Stairs_3k.txt
+#     ...
+# ------------------------#
 ###########################
 # # Output File
 # {
@@ -82,12 +92,15 @@ import json
 
 img_folder_path = '/home/ICT2000/jyang/Documents/Data/ForJing/renderedPhotos'
 cam_folder_path = '/home/ICT2000/jyang/Documents/Data/ForJing/arbitraryCameras_30_adjust'
-sh_folder_path = ''
+sh_folder_path = '/home/ICT2000/jyang/Documents/Data/ForJing/spherical_harmonics'
 output_path = '/home/ICT2000/jyang/Documents/Data/ForJing'
 
 out_json = {
     'frames': []
 }
+
+with open('{}/environment_in_order.txt'.format(sh_folder_path)) as sh_order_file:
+    sh_order = sh_order_file.read().split('\n')
 
 # walk through all images in img_path
 for r, d, f in os.walk(img_folder_path):
@@ -152,13 +165,24 @@ for r, d, f in os.walk(img_folder_path):
                         transform_list.append(row.tolist())
                     return transform_list
 
+                def sh2list(environ):
+                    sh_file_name = sh_order[environ-1]
+                    sh_name = os.path.splitext(sh_file_name)[0]
+                    with open('{}/original_sh/{}.txt'.format(sh_folder_path, sh_name)) as sh_file:
+                        sh_txt = sh_file.read().strip()
+                        sh = np.matrix(sh_txt.replace('\n', ';'))
+                        sh_list = []
+                        for row in np.asarray(sh)[:4]:
+                            sh_list.append(row.tolist())
+                        return sh_list
+
                 out_json['frames'].append({
                     'file_path': os.path.join(r, file),
                     'hwf': hwf2list(res, focal),
                     'transform_matrix': transform2list(transform),
-                    'sh': ''
+                    'sh': sh2list(environ)
                 })
 
-# output to json
-with open('{}/transforms.json'.format(output_path), 'w') as out_file:
-    json.dump(out_json, out_file, indent=4)
+                # output to json
+                with open('{}/transforms.json'.format(output_path), 'w') as out_file:
+                    json.dump(out_json, out_file, indent=4)
